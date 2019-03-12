831cf8281112c57747ffb3e67ac0023660bcad7a
Urhengulas <joheihe@web.de>
2019-03-12 16:11:52 +0100

    moved functions get_site_soup() and parse_for_filter() to separate file scraper.py

---
+++ scraper.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import urllib3
+
+
+def get_site_soup(url):
+    """Get the Soup of a given Url."""
+    
+    # request given url
+    http = urllib3.PoolManager()
+    r = http.request('get', url)
+
+    # format it nicely and return
+    soup = BeautifulSoup(r.data, 'lxml')
+
+    return soup
+
+
+def parse_for_filter(soup, filter=("li", {"class": "ads-ad"})):
+    """Parses the given soup for given filters."""
+
+    # get a list of all the elements, which shall be filtered for
+    specific = soup.find_all(filter[0], filter[1])
+
+    # get list of information about the elements and save each in a dictionary
+    entry_list = []
+    for entry in specific:
+        dic = {
+            "text": entry.find('a').text,
+            "ad_link": entry.find('div', {'class': 'ads-visurl'}).cite.text,
+            "html": entry
+        }
+        entry_list.append(dic)
+
+    return entry_list
