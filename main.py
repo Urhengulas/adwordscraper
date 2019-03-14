23efb42f6a5c3c5ddd84f61e58d44bbcff801e31
Jonathan Freiberger <jonathan@freibergers.de>
2019-03-14 21:06:01 +0100

    added userinput as arguments

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
 import csv
+import sys
 from KeywordAdsScraper import KeywordAdsScraper
+
+def main():
+
+#check user input
+    if (len(sys.argv) > 3 or len(sys.argv) < 1):
+        print("usage: main.py inputfile.csv [outputfile.csv]")
+        return 0 # stop the program
+
+    inputfile = sys.argv[1]
+    outputfile = sys.argv[2] if len(sys.argv) > 2 else "ads.csv"
+
+    # getting the keywords from csv file
+    keywords = getKeywordsFromCsv(inputfile)
+
+    # generate an AdWordScraper-Object for each keyword
+    keyword_ads_list = [KeywordAdsScraper(key) for key in keywords]
+
+    # Print all Ads in a structured way
+    saveAdsAsCsv(keyword_ads_list,outputfile)
+
+
+def print_keyword_ads_list(keyword_ads_list):
+    """Print all Ads in a structured way."""
+    for keyword_ad in keyword_ads_list:
+        print("### Keyword: {}".format(keyword_ad.title))
+
+        if not keyword_ad.ad_list:
+            print("There are no ads.")
+        else:
+            for ad in keyword_ad.ad_list:
+                print("#\tText: {}\n\tLink: {}".format(
+                    ad["text"], ad["ad_link"]))
+        print()
 
 
 def getKeywordsFromCsv(csv):
@@ -15,8 +49,7 @@ def getKeywordsFromCsv(csv):
 
     return keywords
 
-
-def saveAdsAsCsv(adslist, filename="ads.csv"):
+def saveAdsAsCsv(adslist, filename):
 
     with open(filename, 'w', newline='') as file:
         writer = csv.writer(file, delimiter=';')
@@ -28,31 +61,6 @@ def saveAdsAsCsv(adslist, filename="ads.csv"):
             #iterate through ad dict
             for ad in adObject.ad_list:
                 writer.writerow([adObject.title] + [ad['text']] + [ad['ad_link']])
-
-
-def main():
-    # getting the keywords from csv file
-    keywords = getKeywordsFromCsv("test.csv")
-
-    # generate an AdWordScraper-Object for each keyword
-    keyword_ads_list = [KeywordAdsScraper(key) for key in keywords]
-
-    # Print all Ads in a structured way
-    saveAdsAsCsv(keyword_ads_list)
-
-
-def print_keyword_ads_list(keyword_ads_list):
-    """Print all Ads in a structured way."""
-    for keyword_ad in keyword_ads_list:
-        print("### Keyword: {}".format(keyword_ad.title))
-
-        if not keyword_ad.ad_list:
-            print("There are no ads.")
-        else:
-            for ad in keyword_ad.ad_list:
-                print("#\tText: {}\n\tLink: {}".format(
-                    ad["text"], ad["ad_link"]))
-        print()
 
 
 if __name__ == "__main__":
