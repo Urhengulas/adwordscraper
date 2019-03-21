2ea4733d0871fae498309e237645db2bcd783c58
Urhengulas <joheihe@web.de>
2019-03-21 01:31:50 +0100

    move csv functions to csv_handler.py

--- main.py
+++ main.py
@@ -46,31 +46,5 @@ def print_keyword_ads_list(keyword_ads_list):
         print()
 
 
-def getKeywordsFromCsv(csv):
-    # reading the csv file
-    with open(csv, 'r') as file:
-        keywords = []
-
-        #sring cleaning
-        for keyword in file:
-            temp = keyword.replace(";", "").replace("\n", "")
-            keywords.append(temp)
-
-        return keywords
-
-def saveAdsAsCsv(adslist, filename):
-
-    with open(filename, 'w', newline='') as file:
-        writer = csv.writer(file, delimiter=';')
-
-        #adding header
-        writer.writerow(['keyword'] + ['text'] + ['link'])
-
-        for adObject in adslist:
-            #iterate through ad dict
-            for ad in adObject.ad_list:
-                writer.writerow([adObject.title] + [ad['text']] + [ad['ad_link']])
-
-
 if __name__ == "__main__":
     main()
