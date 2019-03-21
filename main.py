3ac11aef7db40fe1ccd69f90a2cd487eb5c5843f
Urhengulas <joheihe@web.de>
2019-03-21 01:46:33 +0100

    adapt main.py to all new changes and autoformat it

--- main.py
+++ main.py
@@ -1,40 +1,46 @@
-import csv
 import sys
-from KeywordAdsScraper import KeywordAdsScraper
+from SearchAdsScraper import SearchAdsScraper
+import csv_handler
+
 
 def main():
+    """
+        make the scraper via CLI available
 
-#check user input
+        usage:
+            python main.py inputfile.csv [outputfile.csv]    
+    """
 
-    #check if more than two arguements are given
+    # check user input
+    # check if more than two arguements are given
     if (len(sys.argv) > 3):
-        print("usage: main.py inputfile.csv [outputfile.csv]")
-        return 0 # stop the program
+        print("usage: python main.py inputfile.csv [outputfile.csv]")
+        return 0  # stop the program
 
     try:
         inputfile = sys.argv[1]
         outputfile = sys.argv[2] if len(sys.argv) > 2 else "ads.csv"
     except IndexError:
-        print("usage: main.py inputfile.csv [outputfile.csv]")
-        return 0 # stop the program
+        print("usage: python main.py inputfile.csv [outputfile.csv]")
+        return 0  # stop the program
 
     # adds .csv appendix if not done by the user
-    if ".csv" not in inputfile: inputfile += '.csv'
-    if ".csv" not in outputfile: outputfile += '.csv'
+    if ".csv" not in inputfile:
+        inputfile += '.csv'
+    if ".csv" not in outputfile:
+        outputfile += '.csv'
 
-    # getting the keywords from csv file
-    keywords = getKeywordsFromCsv(inputfile)
+    # generate the SearchAdsScraper-Object with the <inputfile>
+    Scraper = SearchAdsScraper(inputfile)
 
-    # generate an AdWordScraper-Object for each keyword
-    keyword_ads_list = [KeywordAdsScraper(key) for key in keywords]
-
-    # Print all Ads in a structured way
-    saveAdsAsCsv(keyword_ads_list,outputfile)
-
-
+    # let it scrape
+    Scraper.scrape()
 
     # print result
     print(Scraper)
+
+    # save results to <outputfile>
+    Scraper.save_to_csv(outputfile)
 
 
 if __name__ == "__main__":
