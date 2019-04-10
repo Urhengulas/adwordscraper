8267a380b05bb6ce81e326993df3a7c9e26f9c24
Urhengulas <joheihe@web.de>
2019-04-10 23:29:40 +0200

    (lint) shorten line

--- tests/test.py
+++ tests/test.py
@@ -14,10 +14,13 @@ def test_parse_for_filter():
 
     wanted = ([{'text': 'I am an ad. Yay.',
                 'ad_link': 'www.giffits.de/Beutel/Bedrucken',
-                'html': '<li class="ads-ad">\n<a>I am an ad. Yay.</a>\n<div class="ads-visurl">\n<cite class="UdQCqe">www.giffits.de/Beutel/Bedrucken</cite>\n</div>\n</li>'},
+                'html': '<li class="ads-ad">\n<a>I am an ad. Yay.</a>\n<div class="ads-visurl">\n \
+                    <cite class="UdQCqe">www.giffits.de/Beutel/Bedrucken</cite>\n</div>\n</li>'},
                {'text': 'I am also an fancy ad. Woop woop.',
                 'ad_link': 'www.moritz.dev',
-                'html': '<li class="ads-ad">\n<a>I am also an fancy ad. Woop woop.</a>\n<div class="ads-visurl">\n<cite class="UdQCqe">www.moritz.dev</cite>\n</div>\n</li>'}])
+                'html': '<li class="ads-ad">\n<a>I am also an fancy ad. Woop woop.</a>\n<div \
+                    class="ads-visurl">\n<cite class="UdQCqe">www.moritz.dev</cite>\n</div>\n \
+                        </li>'}])
 
     assert ret == wanted, "TestError: scraper.parse_for_filter() didn't pass the ... test."
 
