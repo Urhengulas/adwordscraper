29a3016ab201c1e4d62aba3982f06ad850b254b2
Jonathan Freiberger <jonathan@freibergers.de>
2019-04-17 14:04:43 +0200

    change function name of check_input to check_number_of_arguments

--- cli.py
+++ cli.py
@@ -19,21 +19,23 @@ def main():
 
 
 def handle_input(sys_argv):
-    check_input(sys_argv)
+    check_number_of_arguments(sys_argv)
 
     inputfile, outputfile = get_filenames_from_input(sys_argv)
 
     inputfile = check_csv_extension(inputfile)
+
     outputfile = check_csv_extension(outputfile)
 
     return inputfile, outputfile
 
 
-def check_input(sys_argv):
+def check_number_of_arguments(sys_argv):
     """check if more than two arguements are given"""
 
     assert (len(sys_argv) >= 1 and len(sys_argv) <= 3), \
         "usage: python cli.py inputfile.csv [outputfile.csv]"
+
 
 
 def get_filenames_from_input(sys_argv):
