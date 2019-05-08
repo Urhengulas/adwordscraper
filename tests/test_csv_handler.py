4e7b3076f7b4fcc00ce7474e0c78915f18bd68eb
Urhengulas <joheihe@web.de>
2019-05-08 11:45:40 +0200

    Expand test for adscraper.csv_handler
    Changes:
    * add test to check if get_keywords() extracts the right keywords

--- tests/test_csv_handler.py
+++ tests/test_csv_handler.py
@@ -9,3 +9,8 @@ def test_get_keywords():
     # test if Error gets raised when file not exists
     with pytest.raises(FileNotFoundError):
         csv_handler.get_keywords('foo.csv')
+
+    # test if right keywords get extracted
+    keywords = csv_handler.get_keywords('tests/test.csv')
+    test_keywords = ["This ", "Are", "TEST", "keywords"]
+    assert keywords == test_keywords
