c4771c91a27bb8541da50d2c31f538481d62c565
Urhengulas <joheihe@web.de>
2019-05-08 11:44:59 +0200

    Let adscraper.csv_handler tests use dummy data

--- tests/test_csv_handler.py
+++ tests/test_csv_handler.py
@@ -4,7 +4,7 @@ from adscraper import csv_handler
 
 def test_get_keywords():
     # test if file gets open correctly
-    assert csv_handler.get_keywords('data/keywords.csv')
+    assert csv_handler.get_keywords('tests/test.csv')
 
     # test if Error gets raised when file not exists
     with pytest.raises(FileNotFoundError):
